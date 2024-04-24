05d66e56d1d782f27d108b8f6a009cfa4fefb302
Goran Marjanovic <gm.xsmilex@gmail.com>
2024-04-24 19:23:35 +0200

    added

--- nizoviZadatak.js
+++ nizoviZadatak.js
@@ -55,13 +55,18 @@ names.sort();
 //6
 console.log(names.join());
 
-var brojcaniNiz = [1, 2, , 3, 4, , 5, 6, 7];
+var brojcaniNiz = [1, 2, , 3, 4, , 5, 'test', [1, 2, 3], 6, 7, true];
 // zbroji sve članove niza
 
 var suma = 0;
 for (var x = 0; x < brojcaniNiz.length; x++) {
     if (typeof brojcaniNiz[x] != 'number') {
-        console.log('Element na indexu ' + x + ' nije broj pa ga preskačemo');
+        console.log(
+            'Element na indexu ' +
+                x +
+                ' nije broj pa ga preskačemo nego je = ' +
+                typeof brojcaniNiz[x],
+        );
         continue;
     }
     suma += brojcaniNiz[x];
@@ -93,3 +98,30 @@ for (var i = 0; i < elementiBr.length; i++) {
     }
 }
 console.log(ispisi);
+
+var namesLong = [
+    'John',
+    'Jane',
+    'Donatello',
+    'Bob',
+    ,
+    1,
+    [2, 3, 4],
+    'Mike',
+    'Leonardo',
+];
+var duljina = 0;
+var najduzeIme = '';
+for (var ime of namesLong) {
+    //console.log('Evaluiram Ime:' + ime);
+    if (typeof ime !== 'string') {
+        continue;
+    }
+    var duljinaImena = ime.length;
+    if (duljinaImena > duljina) {
+        duljina = duljinaImena;
+        najduzeIme = ime;
+    }
+}
+console.log('Duljina: ' + duljina);
+console.log('Najduze ime je: ' + najduzeIme);
